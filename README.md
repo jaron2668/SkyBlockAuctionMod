420f10595485bd5d3db86c11498c9eede8106db8
jaron2668 <jaron2668@gmail.com>
2026-09-11 14:24:22 +0200

    initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# SkyBlock Auction Mod
---
+++ src/client/java/io/github/jaron2668/client/SkyBlockAuctionModClient.java
@@ -0,0 +1,10 @@
+package io.github.jaron2668.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkyBlockAuctionModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
